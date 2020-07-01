eca8dde1e3998a6b366dbb8806e12bb36727521d
Matteo <matteopieroni6@gmail.com>
2020-07-01 12:39:32 +0100

    Return path working for windows and unix

--- src/utils/getBasePath.ts
+++ src/utils/getBasePath.ts
@@ -1,5 +1,4 @@
 export function getBasePath(path: string) {
-	const finalPath = path.replace(/^file\:(\/)+/, '').replace(/(\/|\\)(package-lock\.json|yarn.lock)/, '');
-	const isWindowsPath = !path.match(/\//)?.length || path.match(/\//)?.length === 0;
-	return isWindowsPath ? finalPath : `/${finalPath}`;
+	const finalPath = path.replace(/(\/|\\)(package-lock\.json|yarn.lock)/, '');
+	return finalPath;
 }
