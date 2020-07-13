6359802ec77ac2572022ed7f102fdc421cb86534
Matteo <matteopieroni6@gmail.com>
2020-07-13 20:34:47 +0100

    Added step to check version

---
+++ check-version.js
@@ -0,0 +1,33 @@
+const { exec } = require('child_process');
+const { version } = require('./package.json');
+
+const command = 'vsce show matteopieroni.refresh-npm-packages';
+
+exec(command, (err, stdout) => {
+  if (err) {
+    //some err occurred
+		console.error(err);
+		
+		process.exit(1);
+  } else {
+		try {
+			const isVersionString = stdout.match(/Version:(\s)+[0-9|.]+(\n)?/gm);
+			const versionString = isVersionString !== null ? isVersionString[0] : null;
+			
+			if (typeof versionString === 'string') {
+				const publishedVersion = versionString.replace(/Version:(\s)+/g, '');
+				const isSameVersion = publishedVersion === version;
+				
+				if (!isSameVersion) {
+					console.log('New version detected', version);
+					return process.exit(0);
+				};
+
+				throw new Error('Skip publishing');
+			}
+		} catch(e) {
+			console.error(e);
+			process.exit(1);
+		}
+  }
+});
